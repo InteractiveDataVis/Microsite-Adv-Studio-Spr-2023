663cd721c2d455d5a54b420ba0b932fd328a284a
Aucher Serr <aserr@atlassian.com>
2022-05-23 20:35:08 -0400

    update link for course description

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,9 @@
-import { select } from "d3";
-import { keys, appConfig, courseDescription } from "../utils/constants";
+import { select } from 'd3';
+import { keys, appConfig, courseDescription } from '../utils/constants';
 
-import GithubIcon from "../assets/github.svg";
-import UpArrow from "../assets/arrow_upward-24px.svg";
-import LaunchIcon from "../assets/launch-24px.svg";
+import GithubIcon from '../assets/github.svg';
+import UpArrow from '../assets/arrow_upward-24px.svg';
+import LaunchIcon from '../assets/launch-24px.svg';
 
 const { getHash } = appConfig;
 const { desc, github, name, title, portfolio, prevImg1, prevImg2 } = keys;
@@ -15,70 +15,68 @@ export class Content {
   }
 
   init() {
-    this.contentWrapper = select("#grid")
-      .append("div")
-      .attr("id", "content-wrapper");
+    this.contentWrapper = select('#grid').append('div').attr('id', 'content-wrapper');
 
     this.students = this.contentWrapper
-      .selectAll("div.student")
+      .selectAll('div.student')
       .data(this.data)
-      .join("div")
-      .attr("class", "student")
-      .attr("id", (d) => getHash(d[name]));
+      .join('div')
+      .attr('class', 'student')
+      .attr('id', (d) => getHash(d[name]));
 
     // name
     this.students
-      .append("div")
-      .attr("class", "student-name")
+      .append('div')
+      .attr('class', 'student-name')
       .text((d) => d[name]);
 
     const socials = [
-      { key: github, icon: GithubIcon, title: "Open Github" },
-      { key: portfolio, icon: LaunchIcon, title: "See Portfolio" },
+      { key: github, icon: GithubIcon, title: 'Open Github' },
+      { key: portfolio, icon: LaunchIcon, title: 'See Portfolio' },
     ];
 
     // socials
     this.students
-      .selectAll("student-socials")
+      .selectAll('student-socials')
       .data((d) =>
         socials.map((s) => ({
           ...s,
           link: d[s.key],
           title: d[title] ? `See ${d[title]}` : s.title,
-        }))
+        })),
       )
-      .join("a")
+      .join('a')
       .filter((d) => d.link)
-      .attr("class", "student-socials")
-      .attr("href", (d) => d.link)
-      .attr("title", (d) => d.title)
-      .attr("target", "_blank")
-      .attr("rel", "noopener")
-      .append("img")
-      .attr("class", (d) => d.key)
-      .attr("src", (d) => d.icon);
+      .attr('class', 'student-socials')
+      .attr('href', (d) => d.link)
+      .attr('title', (d) => d.title)
+      .attr('target', '_blank')
+      .attr('rel', 'noopener')
+      .append('img')
+      .attr('class', (d) => d.key)
+      .attr('src', (d) => d.icon);
 
     // description
-    const work = this.students.append("div").attr("class", "student-work-grid");
+    const work = this.students.append('div').attr('class', 'student-work-grid');
 
     work
-      .append("div")
-      .attr("class", "student-desc")
+      .append('div')
+      .attr('class', 'student-desc')
       .text((d) => d[desc])
-      .append("a")
-      .attr("href", (d) => d[portfolio])
-      .attr("target", "_blank")
-      .attr("rel", "noopener")
-      .append("div")
-      .attr("class", "project-title")
-      .html((d) => d[title])
+      .append('a')
+      .attr('href', (d) => d[portfolio])
+      .attr('target', '_blank')
+      .attr('rel', 'noopener')
+      .append('div')
+      .attr('class', 'project-title')
+      .html((d) => d[title]);
 
     const images = [prevImg1, prevImg2];
 
     work
-      .append("div")
-      .attr("class", "student-images")
-      .selectAll(".student-img")
+      .append('div')
+      .attr('class', 'student-images')
+      .selectAll('.student-img')
       .data((d) =>
         images
           .map((i) => ({
@@ -86,42 +84,49 @@ export class Content {
             [portfolio]: d[portfolio],
             [title]: d[title],
           }))
-          .filter((d) => d.img)
+          .filter((d) => d.img),
       )
-      .join("a")
-      .attr("class", "student-img")
-      .attr("href", (d) => d[portfolio])
-      .attr("title", (d) => d[title])
-      .attr("target", "_blank")
-      .attr("rel", "noopener")
-      .append("img")
-      .attr("loading", "lazy")
-      .attr("src", (d) => d.img);
+      .join('a')
+      .attr('class', 'student-img')
+      .attr('href', (d) => d[portfolio])
+      .attr('title', (d) => d[title])
+      .attr('target', '_blank')
+      .attr('rel', 'noopener')
+      .append('img')
+      .attr('loading', 'lazy')
+      .attr('src', (d) => d.img);
 
     this.students
-      .append("span")
-      .attr("class", "scroll-to-top")
-      .append("img")
-      .attr("class", "up-arrow")
-      .attr("src", UpArrow)
-      .on("click", this.scrollToTop);
+      .append('span')
+      .attr('class', 'scroll-to-top')
+      .append('img')
+      .attr('class', 'up-arrow')
+      .attr('src', UpArrow)
+      .on('click', this.scrollToTop);
 
-    this.credits = this.contentWrapper.append("div")
-      .attr("id", "page-credits")
+    this.credits = this.contentWrapper.append('div').attr('id', 'page-credits');
 
-    this.credits.append("h3").text("Course Description")
-    this.credits.append("a")
-      .attr("href", "https://gc.cuny.edu/Page-Elements/Academics-Research-Centers-Initiatives/Masters-Programs/Data-Analysis-and-Visualization/Courses/Fall-2020")
-      .append("h2").attr("class", "title").text("Advanced Interactive Data Visualization Studio")
-    this.credits.selectAll(".description")
+    this.credits.append('h3').text('Course Description');
+    this.credits
+      .append('a')
+      .attr(
+        'href',
+        'https://www.gc.cuny.edu/data-analysis-and-visualization/curriculum-and-degree-information/courses',
+      )
+      .attr('target', 'blank')
+      .append('h2')
+      .attr('class', 'title')
+      .text('Advanced Interactive Data Visualization Studio');
+    this.credits
+      .selectAll('.description')
       .data(courseDescription)
-      .join("p")
-      .attr("class", 'description')
-      .html(d => d)
+      .join('p')
+      .attr('class', 'description')
+      .html((d) => d);
   }
 
   scrollToTop() {
-    select(`body`).node().scrollIntoView({ behavior: "smooth" });
+    select(`body`).node().scrollIntoView({ behavior: 'smooth' });
   }
 
   get node() {
@@ -129,10 +134,10 @@ export class Content {
   }
 
   makeFixed(headerHeight = 250) {
-    console.log("in make fixed");
+    console.log('in make fixed');
     this.contentWrapper
-      .style("position", "sticky")
-      .style("height", `${window.innerHeight - headerHeight}px`)
-      .style("top", `${headerHeight}px`);
+      .style('position', 'sticky')
+      .style('height', `${window.innerHeight - headerHeight}px`)
+      .style('top', `${headerHeight}px`);
   }
 }
